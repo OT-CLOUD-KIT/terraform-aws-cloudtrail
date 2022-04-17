67afdd3b9ca60fcdf8dbd514300669a244b519ec
Prakash Jha <prakash.jha@opstree.com>
2022-04-17 17:58:42 +0530

    Added feature for advanced event selector

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "id" {
     description = "ID of cloudtrail"
-    value = aws_cloudtrail.default.id
+    value = aws_cloudtrail.cloudtrail.id
 }
 
 output "arn" {
     description = "ARN of cloudtrail"
-    value = aws_cloudtrail.default.arn
+    value = aws_cloudtrail.cloudtrail.arn
 }
 
 output "home_region" {
     description = "Region in which the trail was created"
-    value = aws_cloudtrail.default.home_region
+    value = aws_cloudtrail.cloudtrail.home_region
 }
