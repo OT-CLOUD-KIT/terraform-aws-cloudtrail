bb68539997542303d1d143eec89eb4fe2e2792a8
Prakash Jha <83633203+prakashjha-ot@users.noreply.github.com>
2022-05-06 12:33:00 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,14 @@
 output "id" {
     description = "ID of cloudtrail"
-    value = aws_cloudtrail.cloudtrail.id
+    value = aws_cloudtrail.default.id
 }
 
 output "arn" {
     description = "ARN of cloudtrail"
-    value = aws_cloudtrail.cloudtrail.arn
+    value = aws_cloudtrail.default.arn
 }
 
 output "home_region" {
     description = "Region in which the trail was created"
-    value = aws_cloudtrail.cloudtrail.home_region
+    value = aws_cloudtrail.default.home_region
 }
